17b8605436762b3e587bdff31a2b19a4721b381c
runtee <ekwo.chinonso.01@gmail.com>
2023-12-16 18:16:12 +0100

    added routing

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
